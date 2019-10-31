af26048aa200d17c49bc6e5daea341ba2c1bd1e7
kyledexperience <kyledexperience@gmail.com>
2019-10-31 11:02:31 -0400

    x :x

--- src/co/gc/AntonellasFile.java
+++ src/co/gc/AntonellasFile.java
@@ -3,6 +3,17 @@ package co.gc;
 public class AntonellasFile {
 
 	private String name;
+	private String otherName;
+	
+	
+
+	public String getOtherName() {
+		return otherName;
+	}
+
+	public void setOtherName(String otherName) {
+		this.otherName = otherName;
+	}
 
 	public String getName() {
 		return name;
