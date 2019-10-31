385c7013637d5fe99ad91ac5cf752bf273e184e3
kyledexperience <kyledexperience@gmail.com>
2019-10-31 10:52:33 -0400

    second commit by Kyky

--- src/co/gc/MainApp.java
+++ src/co/gc/MainApp.java
@@ -5,7 +5,7 @@ public class MainApp {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		
-		System.out.println("Hello, my name is Antonella!");
+		System.out.println("Hello, my name is Unlucky!");
 		
 		System.out.println("Happy Halloween!");
 
